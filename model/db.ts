c81868f586571816f78b325f78cf106ba5e37452
Atlan <756777965@qq.com>
2023-03-05 21:12:39 +0800

    chore:调整配置项

--- model/db.ts
+++ model/db.ts
@@ -1,3 +1,19 @@
-import mysql from "mysql";
+const mysql = require("mysql");
 
-const pool = mysql.createPool({});
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "asdfghjkl551,H",
+  database: "atlanbase",
+});
+
+const query = (sql: string, callback: (_err: Error, rows: any) => void) => {
+  pool.getConnection((err: Error, connection: any) => {
+    connection.query(sql, (_err: Error, rows: any) => {
+      callback(_err, rows);
+      connection.release();
+    });
+  });
+};
+
+export default query;
