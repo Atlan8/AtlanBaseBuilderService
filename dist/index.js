ce03c5ae3349ef377e6e851b50e81c48df0b2180
Atlan <756777965@qq.com>
2023-03-14 21:48:43 +0800

    feat:配置解析post请求体

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const router_1 = __importDefault(require("./src/routes/router"));
+const body_parser_1 = __importDefault(require("body-parser"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+// 配置解析表单 POST 请求体，需要在路由挂载之前完成
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
 // 解决跨域问题
 app.all("*", function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3100");
--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import router from "./src/routes/router";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+// 配置解析表单 POST 请求体，需要在路由挂载之前完成
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // 解决跨域问题
 app.all("*", function (req, res, next) {
