8058042e2f54cbbe0fdbdab528e17851bff3ef9d
Atlan <756777965@qq.com>
2023-05-11 21:25:34 +0800

    fix:修复筛选条件

--- dist/src/routes/assemble/index.js
+++ dist/src/routes/assemble/index.js
@@ -18,7 +18,8 @@ const db_1 = __importDefault(require("../../../model/db"));
 const common_1 = require("./common");
 const assemble = express_1.default.Router();
 const sql = {
-    queryAssemble: `select a.id, a.name, a.total, a.timestramp, a.datetime, t.info as cpu, m.info as motherboard, me.info as memory, r.info as radiator, h.info as hardDiskList, g.info as graphicsCard, p.info as powerSupply, c.info as chassis, f.info as fan from assemble a left join cpu t on a.cpu = t.ID left join motherboard m on a.motherboard = m.id left join memory me on a.memory = me.id left join radiator r on a.radiator = r.id left join hard_disk h on a.hardDiskList = h.id left join graphics_card g on a.graphicsCard = g.id left join power_supply p on a.powerSupply = p.id left join chassis c on a.chassis = c.id left join fan f on a.fan = f.id where a.name LIKE ?`,
+    queryAssemble: `select a.id, a.name, a.total, a.timestramp, a.datetime, t.info as cpu, m.info as motherboard, me.info as memory, r.info as radiator, h.info as hardDiskList, g.info as graphicsCard, p.info as powerSupply, c.info as chassis, f.info as fan from assemble a left join cpu t on a.cpu = t.ID left join motherboard m on a.motherboard = m.id left join memory me on a.memory = me.id left join radiator r on a.radiator = r.id left join hard_disk h on a.hardDiskList = h.id left join graphics_card g on a.graphicsCard = g.id left join power_supply p on a.powerSupply = p.id left join chassis c on a.chassis = c.id left join fan f on a.fan = f.id`,
+    queryAssembleByKeyword: `select a.id, a.name, a.total, a.timestramp, a.datetime, t.info as cpu, m.info as motherboard, me.info as memory, r.info as radiator, h.info as hardDiskList, g.info as graphicsCard, p.info as powerSupply, c.info as chassis, f.info as fan from assemble a left join cpu t on a.cpu = t.ID left join motherboard m on a.motherboard = m.id left join memory me on a.memory = me.id left join radiator r on a.radiator = r.id left join hard_disk h on a.hardDiskList = h.id left join graphics_card g on a.graphicsCard = g.id left join power_supply p on a.powerSupply = p.id left join chassis c on a.chassis = c.id left join fan f on a.fan = f.id where a.name LIKE ?`,
     queryAssembleById: `select a.id, a.name, a.total, a.timestramp, a.datetime, t.info as cpu, m.info as motherboard, me.info as memory, r.info as radiator, h.info as hardDiskList, g.info as graphicsCard, p.info as powerSupply, c.info as chassis, f.info as fan from assemble a left join cpu t on a.cpu = t.ID left join motherboard m on a.motherboard = m.id left join memory me on a.memory = me.id left join radiator r on a.radiator = r.id left join hard_disk h on a.hardDiskList = h.id left join graphics_card g on a.graphicsCard = g.id left join power_supply p on a.powerSupply = p.id left join chassis c on a.chassis = c.id left join fan f on a.fan = f.id where a.id = ?`,
     insertCpu: `insert into cpu (name, price, link, info) value (?,?,?,?)`,
 };
@@ -26,7 +27,9 @@ assemble.get("/api/getAssembleList", (req, res) => __awaiter(void 0, void 0, voi
     // res.setHeader("Access-Control-Allow-Origin", "*");
     const body = req.query;
     if (!body.id) {
-        const rows = yield (0, db_1.default)(sql.queryAssemble, [`%${body.keyword}%`]);
+        const _sqlStr = !!body.keyword ? sql.queryAssemble + " where a.name LIKE ?" : sql.queryAssemble;
+        const _searchKey = !!body.keyword ? [`%${body.keyword}%`] : [];
+        const rows = yield (0, db_1.default)(_sqlStr, _searchKey);
         console.warn(rows);
         const _rows = (0, common_1.parseRows)(rows);
         res.send({
@@ -64,12 +67,20 @@ assemble.get("/api/getAssembleListById", (req, res) => __awaiter(void 0, void 0,
         });
     }
 }));
-assemble.post("/api/assemble/edit", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+assemble.post("/api/assemble/create", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const body = req.body;
     console.log("---> post 请求体: ", body);
     if (body) {
         const result = yield (0, db_1.default)(sql.insertCpu, [body.cpu.name, body.cpu.price, body.cpu.link, JSON.stringify(body.cpu)]);
         console.log("cpu插入成功！", result);
     }
+}));
+assemble.post("/api/assemble/edit", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const body = req.body;
+    console.log("---> post 请求体: ", body);
+    // if (body) {
+    //   const result = await query(sql.insertCpu, [body.cpu.name, body.cpu.price, body.cpu.link, JSON.stringify(body.cpu)]);
+    //   console.log("cpu插入成功！", result);
+    // }
 }));
 exports.default = assemble;
--- src/routes/assemble/index.ts
+++ src/routes/assemble/index.ts
@@ -7,7 +7,8 @@ import { parseRows } from "./common";
 const assemble = express.Router();
 
 const sql = {
-  queryAssemble: `select a.id, a.name, a.total, a.timestramp, a.datetime, t.info as cpu, m.info as motherboard, me.info as memory, r.info as radiator, h.info as hardDiskList, g.info as graphicsCard, p.info as powerSupply, c.info as chassis, f.info as fan from assemble a left join cpu t on a.cpu = t.ID left join motherboard m on a.motherboard = m.id left join memory me on a.memory = me.id left join radiator r on a.radiator = r.id left join hard_disk h on a.hardDiskList = h.id left join graphics_card g on a.graphicsCard = g.id left join power_supply p on a.powerSupply = p.id left join chassis c on a.chassis = c.id left join fan f on a.fan = f.id where a.name LIKE ?`,
+  queryAssemble: `select a.id, a.name, a.total, a.timestramp, a.datetime, t.info as cpu, m.info as motherboard, me.info as memory, r.info as radiator, h.info as hardDiskList, g.info as graphicsCard, p.info as powerSupply, c.info as chassis, f.info as fan from assemble a left join cpu t on a.cpu = t.ID left join motherboard m on a.motherboard = m.id left join memory me on a.memory = me.id left join radiator r on a.radiator = r.id left join hard_disk h on a.hardDiskList = h.id left join graphics_card g on a.graphicsCard = g.id left join power_supply p on a.powerSupply = p.id left join chassis c on a.chassis = c.id left join fan f on a.fan = f.id`,
+  queryAssembleByKeyword: `select a.id, a.name, a.total, a.timestramp, a.datetime, t.info as cpu, m.info as motherboard, me.info as memory, r.info as radiator, h.info as hardDiskList, g.info as graphicsCard, p.info as powerSupply, c.info as chassis, f.info as fan from assemble a left join cpu t on a.cpu = t.ID left join motherboard m on a.motherboard = m.id left join memory me on a.memory = me.id left join radiator r on a.radiator = r.id left join hard_disk h on a.hardDiskList = h.id left join graphics_card g on a.graphicsCard = g.id left join power_supply p on a.powerSupply = p.id left join chassis c on a.chassis = c.id left join fan f on a.fan = f.id where a.name LIKE ?`,
   queryAssembleById: `select a.id, a.name, a.total, a.timestramp, a.datetime, t.info as cpu, m.info as motherboard, me.info as memory, r.info as radiator, h.info as hardDiskList, g.info as graphicsCard, p.info as powerSupply, c.info as chassis, f.info as fan from assemble a left join cpu t on a.cpu = t.ID left join motherboard m on a.motherboard = m.id left join memory me on a.memory = me.id left join radiator r on a.radiator = r.id left join hard_disk h on a.hardDiskList = h.id left join graphics_card g on a.graphicsCard = g.id left join power_supply p on a.powerSupply = p.id left join chassis c on a.chassis = c.id left join fan f on a.fan = f.id where a.id = ?`,
   insertCpu: `insert into cpu (name, price, link, info) value (?,?,?,?)`,
 };
@@ -17,7 +18,9 @@ assemble.get("/api/getAssembleList", async (req, res) => {
 
   const body = req.query;
   if (!body.id) {
-    const rows = await query<AssembleInfo[]>(sql.queryAssemble, [`%${body.keyword}%`]);
+    const _sqlStr = !!body.keyword ? sql.queryAssemble + " where a.name LIKE ?" : sql.queryAssemble;
+    const _searchKey = !!body.keyword ? [`%${body.keyword}%`] : [];
+    const rows = await query<AssembleInfo[]>(_sqlStr, _searchKey);
     console.warn(rows);
     const _rows = parseRows(rows);
     res.send({
